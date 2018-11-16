4825e43d80c1c3cb10d2f625bf4d40f7369ef8ab
Justin Lane <jclane@users.noreply.github.com>
2018-11-16 17:22:06 -0500

    Create quests.py

---
+++ quests.py
@@ -0,0 +1,9 @@
+from logic import Quest
+
+quest = Quest()
+quest.id = 0
+quest.name = "Enter the castle"
+quest.description = "Find a way into the castle."
+quest.reward_xp=9999
+quest.reward_gold=9999
+quest.reward_table=["pie", "cake", "stone"]
