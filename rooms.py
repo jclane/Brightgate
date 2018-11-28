210446bae3eb62667e2a9451198e4a410f6ab631
Justin Lane <jclane@users.noreply.github.com>
2018-11-28 12:37:58 -0500

    Changed variable name for mob weapons

--- rooms.py
+++ rooms.py
@@ -1,6 +1,7 @@
 from random import randint
 
 from logic import Room, Monster
+import weapons as wep
 
 entry_gate = Room("ROM_ENTRY_GATE", "Entry Gate")
 entry_gate.description = (
@@ -15,6 +16,9 @@ entry_gate.mobs = [
     Monster("MOB_ORK", "Orc", "One tough hombre", 15, 15, 5, 5, randint(0, 3))
 ]
 
+entry_gate.mobs[0].inventory.add(wep.rusty_dagger, 1)
+entry_gate.mobs[1].inventory.add(wep.rusty_dagger, 1)
+
 court_yard = Room("ROM_COURTYARD", "The Courtyard")
 court_yard.description = (
     "A castle courtyard."
@@ -23,4 +27,3 @@ court_yard.ext_description = (
     "\nNo overgrown vegetation in site."
 )
 court_yard.exits = {"South":"ROM_ENTRY_GATE"}
-
